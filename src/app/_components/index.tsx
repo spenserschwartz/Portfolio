4ffddb9d6ed25eae6fa403cf918d45ac5ee51c44
Spenser Schwartz <spensernet@gmail.com>
2024-04-05 23:48:44 -0700

    Header

--- src/app/_components/index.tsx
+++ src/app/_components/index.tsx
@@ -1,5 +1,13 @@
 import { Container, ContainerInner, ContainerOuter } from "./Container";
 import Footer from "./Footer";
+import { Header } from "./Header";
 import { ThemeProvider } from "./ThemeProvider";
 
-export { Container, ContainerInner, ContainerOuter, Footer, ThemeProvider };
+export {
+  Container,
+  ContainerInner,
+  ContainerOuter,
+  Footer,
+  Header,
+  ThemeProvider,
+};
