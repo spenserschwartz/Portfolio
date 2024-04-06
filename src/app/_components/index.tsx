80de94adb21cf5555cc2bcfb22d0c12a18e1607a
Spenser Schwartz <spensernet@gmail.com>
2024-04-06 00:09:35 -0700

    Logo

--- src/app/_components/index.tsx
+++ src/app/_components/index.tsx
@@ -2,6 +2,7 @@ import About from "./About";
 import { Container, ContainerInner, ContainerOuter } from "./Container";
 import Footer from "./Footer";
 import { Header } from "./Header";
+import Logo from "./Logo";
 import Photos from "./Photos";
 import TechStack from "./TechStack";
 import { ThemeProvider } from "./ThemeProvider";
@@ -13,6 +14,7 @@ export {
   ContainerOuter,
   Footer,
   Header,
+  Logo,
   Photos,
   TechStack,
   ThemeProvider,
