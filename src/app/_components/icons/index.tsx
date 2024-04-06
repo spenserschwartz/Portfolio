481d345b104695cfe9988e1abbb8ced200f0bd3c
Spenser Schwartz <spensernet@gmail.com>
2024-04-06 00:16:30 -0700

    barrel import icons

--- src/app/_components/icons/index.tsx
+++ src/app/_components/icons/index.tsx
@@ -1,4 +1,9 @@
+import ArrowDownIcon from "./ArrowDown";
+import BriefcaseIcon from "./Briefcase";
+import GitHubIcon from "./Github";
 import GraphQLIcon from "./GraphQL";
+import LinkedInIcon from "./LinkedIn";
+import MailIcon from "./Mail";
 import NextIcon from "./Next";
 import NodeIcon from "./Node";
 import ReactIcon from "./React";
@@ -6,7 +11,12 @@ import TailwindIcon from "./Tailwind";
 import TypeScriptIcon from "./TypeScript";
 
 export {
+  ArrowDownIcon,
+  BriefcaseIcon,
+  GitHubIcon,
   GraphQLIcon,
+  LinkedInIcon,
+  MailIcon,
   NextIcon,
   NodeIcon,
   ReactIcon,
