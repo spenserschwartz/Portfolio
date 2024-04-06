49478e87cb85d42ea7384e5ee54e6474cb4ee513
Spenser Schwartz <spensernet@gmail.com>
2024-04-06 00:24:23 -0700

    add image hostnames

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "travelperfect-bucket.s3.us-west-1.amazonaws.com" },
+      { hostname: "travelperfect.io" },
+    ],
+  },
+};
 
 export default nextConfig;
