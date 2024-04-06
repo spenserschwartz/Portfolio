b3e34e52bf71fb436e8e9e72c53a913db5ac0f9b
Spenser Schwartz <spensernet@gmail.com>
2024-04-06 00:07:15 -0700

    TechStack

--- src/app/_components/index.tsx
+++ src/app/_components/index.tsx
@@ -3,6 +3,7 @@ import { Container, ContainerInner, ContainerOuter } from "./Container";
 import Footer from "./Footer";
 import { Header } from "./Header";
 import Photos from "./Photos";
+import TechStack from "./TechStack";
 import { ThemeProvider } from "./ThemeProvider";
 
 export {
@@ -13,5 +14,6 @@ export {
   Footer,
   Header,
   Photos,
+  TechStack,
   ThemeProvider,
 };
