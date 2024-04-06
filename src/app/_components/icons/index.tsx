9b5ebe4d5618652d3d123e5e78393ce11fa6a3fc
Spenser Schwartz <spensernet@gmail.com>
2024-04-06 00:06:38 -0700

    TypeScriptIcon

--- src/app/_components/icons/index.tsx
+++ src/app/_components/icons/index.tsx
@@ -2,5 +2,14 @@ import GraphQLIcon from "./GraphQL";
 import NextIcon from "./Next";
 import NodeIcon from "./Node";
 import ReactIcon from "./React";
+import TailwindIcon from "./Tailwind";
+import TypeScriptIcon from "./TypeScript";
 
-export { GraphQLIcon, NextIcon, NodeIcon, ReactIcon };
+export {
+  GraphQLIcon,
+  NextIcon,
+  NodeIcon,
+  ReactIcon,
+  TailwindIcon,
+  TypeScriptIcon,
+};
