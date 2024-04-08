ddd1c621ba1286cd1332c1f7e741acc4e5bc2299
Spenser Schwartz <spensernet@gmail.com>
2024-04-08 15:39:43 -0700

    Link to a tag, dark mode on logo text

--- src/app/_components/HeaderLogo.tsx
+++ src/app/_components/HeaderLogo.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { forwardRef } from "react";
 import { Logo } from "./index";
 
@@ -9,25 +8,24 @@ interface HeaderLogoProps {
 
 const HeaderLogo = forwardRef<HTMLDivElement, HeaderLogoProps>(() => {
   return (
-    <Link
+    <a
       className="flex items-center align-middle md:justify-start"
-      href="/"
+      href="https://www.travelperfect.io"
+      target="_blank"
       aria-label="TravelPerfect"
-      passHref
-      legacyBehavior
     >
       <div className="flex cursor-pointer items-center align-middle md:justify-start">
         <Logo />
 
         {/* Title */}
         <h1 className="leading-tigher flex items-center text-3xl font-extrabold tracking-tighter md:text-4xl">
-          Travel
+          <span className="text-zinc-900 dark:text-white">Travel</span>
           <span className="bg-gradient-to-r from-purple-500 to-violet-400 bg-clip-text text-transparent">
             Perfect
           </span>
         </h1>
       </div>
-    </Link>
+    </a>
   );
 });
 HeaderLogo.displayName = "HeaderLogo";
